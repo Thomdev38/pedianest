aa064520f2d80c91492035c8789c30b79018ddbd
Thomdev38 <142212013+Thomdev38@users.noreply.github.com>
2024-11-27 13:50:48 +0400

    suppression anglais
    mise hors service bi langue
    suppression du onboarding

--- lib/french/homepagefr.dart
+++ lib/french/homepagefr.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+
 import 'package:pedianesth/french/posologiecalculator.dart';
 
 class Homefrench extends StatefulWidget {
@@ -14,57 +14,10 @@ class _HomefrenchState extends State<Homefrench> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      padding: const EdgeInsets.only(top: 20, bottom: 20, right: 20, left: 20),
-      child: OnBoardingSlider(
-        headerBackgroundColor: Colors.white,
-        finishButtonText: "Accédez à l'outils",
-        onFinish: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-                pageBuilder: (_, __, ___) => const PosologieCalculatorScreen()),
-          );
-        },
-        finishButtonStyle: const FinishButtonStyle(
-          backgroundColor: Color.fromARGB(225, 86, 125, 199),
-        ),
-        background: [
-          Image.asset('assets/images/slide_1.jpeg'),
-          Image.asset('assets/images/slide_2.jpeg'),
-          Image.asset('assets/images/slide_1.jpeg'),
-        ],
-        totalPage: 3,
-        speed: 1.8,
-        pageBodies: [
-          const Column(
-            children: <Widget>[
-              SizedBox(
-                height: 480,
-              ),
-              Text("entrez caracteristique patient"),
-            ],
-          ),
-          const Column(
-            children: <Widget>[
-              SizedBox(
-                height: 480,
-              ),
-              Text('preparer salle'),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 480,
-                ),
-                Text('anesthésie'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ));
+            padding:
+                const EdgeInsets.only(top: 20, bottom: 20, right: 20, left: 20),
+            child: (Text(
+              "text page accueil",
+            ))));
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pedianesth/english/mainen.dart';
-import 'package:pedianesth/french/homepagefr.dart';
-import 'package:pedianesth/french/mainfr.dart';
-import 'package:pedianesth/main.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -15,36 +11,11 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.cyan,
         body: Container(
             padding:
-                const EdgeInsets.only(top: 60, bottom: 20, right: 20, left: 20),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: const Text('Francais'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Mainfr()),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                  child: const Text('English'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Mainen()),
-                    );
-                  },
-                ),
-              ],
-            )));
+                const EdgeInsets.only(top: 20, bottom: 20, right: 20, left: 20),
+            child: (Text(
+              "text page accueil",
+            ))));
   }
 }
